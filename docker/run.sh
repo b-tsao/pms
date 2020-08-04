c2c2e1500e32103fcab213d2f060b529a424cefd
Brian Tsao <btsao1790@gmail.com>
2020-08-03 23:05:54 -0400

    docker

--- docker/run.sh
+++ docker/run.sh
@@ -3,10 +3,11 @@
 docker build --file sql/Dockerfile -t pms_sql ..
 docker build --file pms/Dockerfile -t pms ..
 
-PMS_MYSQL_CONTAINER_ID=`docker run -d -p 3306:3306 --name pms_sql pms_sql`
+docker network create pms_net
 
-PMS_MYSQL_CONTAINER_IP_ADDRESS=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $PMS_MYSQL_CONTAINER_ID`
+docker run -d --network pms_net -p 3306:3306 --name pms_sql pms_sql
+docker run -it --rm --network pms_net -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --name pms pms bash
 
-docker run -it --rm -e PMS_MYSQL_CONTAINER_IP_ADDRESS=$PMS_MYSQL_CONTAINER_IP_ADDRESS -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --name pms pms bash
+docker rm -f pms_sql
 
-docker rm -f $PMS_MYSQL_CONTAINER_ID
+docker network rm pms_net
