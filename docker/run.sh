05714809116699792b272b4019245c9b2eb1395d
Brian Tsao <btsao1790@gmail.com>
2020-08-04 01:27:50 -0400

    update

--- docker/run.sh
+++ docker/run.sh
@@ -5,8 +5,8 @@ docker build --file pms/Dockerfile -t pms ..
 
 docker network create pms_net
 
-docker run -d --network pms_net -p 3306:3306 --name pms_sql pms_sql
-docker run -it --rm --network pms_net -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --name pms pms bash
+docker run -d --network pms_net --name pms_sql pms_sql
+docker run -it --rm --network pms_net -p 8383:8383 --name pms pms bash
 
 docker rm -f pms_sql
 
