a881dd8625693f1d96c6b7365720cf15631f69d2
Brian Tsao <btsao1790@gmail.com>
2020-08-05 23:37:02 -0400

    fix

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 mkdir tmp
 javac -cp 'dist/*' -d tmp `find src -iname '*.java'`
 cd tmp
-jar cvf MapleVintage.jar *
+jar cvf odinms.jar *
 cd ..
-mv tmp/MapleVintage.jar dist
+mv tmp/odinms.jar dist
 rm -rf tmp
--- launch_channel.sh
+++ launch_channel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export CLASSPATH=".:dist/odinms.jar:mina-core.jar:slf4j-api.jar:slf4j-jdk14.jar:mysql-connector-java-bin.jar"
+export CLASSPATH=".:dist/odinms.jar:dist/mina-core.jar:dist/slf4j-api.jar:dist/slf4j-jdk14.jar:dist/mysql-connector-java-bin.jar"
 java -Dnet.sf.odinms.recvops=recvops.properties \
 -Dnet.sf.odinms.sendops=sendops.properties \
 -Dnet.sf.odinms.wzpath=. \
