0293de571b2bf10f3591e5180a9e4897e8f30a11
Brian Tsao <btsao@us.ibm.com>
2020-08-05 20:28:41 -0700

    fix

--- src/net/sf/odinms/net/login/handler/LoginPasswordHandler.java
+++ src/net/sf/odinms/net/login/handler/LoginPasswordHandler.java
@@ -1,13 +1,12 @@
 package net.sf.odinms.net.login.handler;
 
 import java.util.Calendar;
+
 import net.sf.odinms.client.MapleCharacter;
 import net.sf.odinms.client.MapleClient;
 import net.sf.odinms.net.MaplePacketHandler;
 import net.sf.odinms.net.channel.ChannelServer;
-import net.sf.odinms.net.login.LoginServer;
 import net.sf.odinms.net.login.LoginWorker;
-import net.sf.odinms.server.AutoRegister;
 import net.sf.odinms.tools.FilePrinter;
 import net.sf.odinms.tools.KoreanDateUtil;
 import net.sf.odinms.tools.MaplePacketCreator;
