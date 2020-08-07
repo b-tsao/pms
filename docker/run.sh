3825fe5352cd99770c655ca02ecb909cb92fc590
Brian <btsao1790@gmail.com>
2020-08-07 13:59:49 -0400

    restructure

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-docker build --file sql/Dockerfile -t pms_sql ..
-docker build --file pms/Dockerfile -t pms ..
+SERVER_DIR="/home/pewpew/pms/server"
+DB_DIR="/home/pewpew/pms"
+
+docker build --file sql/Dockerfile -t pms_sql $SERVER_DIR
+docker build --file server/Dockerfile -t pms $SERVER_DIR
 
 docker network create pms_net
 
-docker run -d --name pms_sql --network pms_net -v /home/pewpew/pms/data:/var/lib/mysql pms_sql
-docker run -it --rm --name pms --network pms_net -p 8484:8484 -p 8787:8787 -p 8585:8585 -p 8586:8586 -p 8587:8587 -p 8588:8588 -v /home/pewpew/pms/dist:/pms/dist -v /home/pewpew/pms/wz:/pms/wz -v /home/pewpew/pms/scripts:/pms/scripts pms bash
+docker run -d --name pms_sql --network pms_net -v ${DB_DIR}/db:/var/lib/mysql pms_sql
+docker run -it --rm --name pms --network pms_net \
+	-p 8484:8484 -p 8787:8787 -p 8585:8585 -p 8586:8586 -p 8587:8587 -p 8588:8588 \
+	-v ${SERVER_DIR}/dist:/pms/dist -v ${SERVER_DIR}/wz:/pms/wz -v ${SERVER_DIR}/scripts:/pms/scripts \
+	pms bash
 
 docker rm -vf pms_sql
 
