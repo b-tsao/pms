3f55c038addb46bdba39f8c541ac07792f775cc5
Brian <btsao1790@gmail.com>
2020-08-07 06:35:17 -0400

    mount volumes

--- docker/run.sh
+++ docker/run.sh
@@ -5,9 +5,9 @@ docker build --file pms/Dockerfile -t pms ..
 
 docker network create pms_net
 
-docker run -d --network pms_net --name pms_sql pms_sql
-docker run -it --rm --network pms_net -p 8484:8484 -p 8787:8787 -p 8585:8585 -p 8586:8586 -p 8587:8587 -p 8588:8588 --name pms pms bash
+docker run -d --name pms_sql --network pms_net -v /home/pewpew/pms/data:/var/lib/mysql pms_sql
+docker run -it --rm --name pms --network pms_net -p 8484:8484 -p 8787:8787 -p 8585:8585 -p 8586:8586 -p 8587:8587 -p 8588:8588 -v /home/pewpew/pms/dist:/pms/dist -v /home/pewpew/pms/wz:/pms/wz -v /home/pewpew/pms/scripts:/pms/scripts pms bash
 
-docker rm -f pms_sql
+docker rm -vf pms_sql
 
 docker network rm pms_net
