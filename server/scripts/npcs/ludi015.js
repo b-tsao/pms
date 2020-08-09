686779465906fcd82bb461a6e8019839971e882e
Brian Tsao <btsao@us.ibm.com>
2020-08-08 20:02:35 -0700

    Update ludi015.js

--- server/scripts/npcs/ludi015.js
+++ server/scripts/npcs/ludi015.js
@@ -7,20 +7,26 @@
  * @author joemama
  */
 
-let floors = [221024400, 221021700]
-
 if (player.hasItem(4001020, 1)) {
-	let selection = npc.askMenu("You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Which of these rocks would you like to teleport to?"
+	let selection = npc.askMenu("You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Which of these rocks would you like to teleport to?\r\n"
 		+ "#L0# First Eos Rock - 100th floor#l\r\n"
 		+ "#L1# Third Eos Rock - 41st floor#l");
+	let prompt;
+	let map;
 	if (selection == 0) {
-		selection = npc.askYesNo("You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bFirst Eos Rock#k at the 100th Floor?");
-		player.loseItem(4001020, 1);
-		player.changeMap(221024400);
+		prompt = "You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bFirst Eos Rock#k at the 100th Floor?";
+		map = 221024400;
 	} else if (selection == 1) {
-		selection = npc.askYesNo("You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bThird Eos Rock#k at the 41st Floor?");
-		player.loseItem(4001020, 1);
-		player.changeMap(221021700);
+		prompt = "You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bThird Eos Rock#k at the 41st Floor?";
+		map = 221021700
+	} else {
+		npc.sayNext("Come back if you're tired of walking.");
+		return;
+	}
+	selection = npc.askYesNo(prompt);
+	if (selection == 1) {
+		player.loseItem(4001020);
+		player.changeMap(map);
 	} else {
 		npc.sayNext("Come back if you're tired of walking.");
 	}
