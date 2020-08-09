16aebd146911476ecc7dc47c1bed01d403d7cf0a
Brian Tsao <btsao@us.ibm.com>
2020-08-08 20:14:36 -0700

    Update ludi015.js

--- server/scripts/npcs/ludi015.js
+++ server/scripts/npcs/ludi015.js
@@ -13,22 +13,20 @@ if (player.hasItem(4001020, 1)) {
 		+ "#L1# Third Eos Rock - 41st floor#l");
 	let prompt;
 	let map;
-	npc.sayNext(selection);
-	// if (selection == 0) {
-	// 	prompt = "You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bFirst Eos Rock#k at the 100th Floor?";
-	// 	map = 221024400;
-	// } else if (selection == 1) {
-	// 	prompt = "You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bThird Eos Rock#k at the 41st Floor?";
-	// 	map = 221021700
-	// }
-	// if (selection == 0 | selection == 1) {
-	// 	selection = npc.askYesNo(prompt);
-	// 	if (selection == 1) {
-	// 		player.loseItem(4001020);
-	// 		player.changeMap(map);
-	// 	}
-	// }
-	// npc.sayNext("Come back if you're tired of walking.");
+	if (selection == 0) {
+		prompt = "You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bFirst Eos Rock#k at the 100th Floor?";
+		map = 221024400;
+	} else if (selection == 1) {
+		prompt = "You can use #bEos Rock Scroll#k to activate #bSecond Eos Rock#k. Will you teleport to #bThird Eos Rock#k at the 41st Floor?";
+		map = 221021700
+	}
+	selection = npc.askYesNo(prompt);
+	if (selection == 1) {
+		player.loseItem(4001020);
+		player.changeMap(map);
+	} else {
+		npc.sayNext("Come back if you're tired of walking.");
+	}
 } else {
 	npc.sayNext("There's a rock that will enable you to teleport to either the #bFirst Eos Rock#k or #bThird Eos Rock#k, but it cannot be activated without the scroll.");
 }
