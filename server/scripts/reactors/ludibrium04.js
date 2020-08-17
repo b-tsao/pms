0eb92a624f591ef58b494fc95ac87f1bec2785fe
9202013 <kha17h@yahoo.com>
2020-08-16 20:45:26 -0700

    added items to boxes for ludi bonus stage

---
+++ server/scripts/reactors/ludibrium04.js
@@ -0,0 +1,34 @@
+/*
+ * ArgonMS MapleStory server emulator written in Java
+ * Copyright (C) 2011-2013  GoldenKevin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * (Reactor 2202003)
+ *  Hidden Street: Abandoned Tower<Bonus> (Map 922011000)
+ *
+ * Ludibrium Party Quest boxes drop rewards in bonus stage.
+ *
+ * @author Neuro
+ */
+
+reactor.dropItems(1102003, 50000, 1102004, 50000, 1102000, 50000, 1102002, 50000, 1102004, 50000, 1102011, 50000, 1102012, 50000, 1102013, 50000, 1102014, 50000, 1032011, 50000, 1032012, 50000, 1032008, 50000, 2070011, 50000, /*Equip*/
+                  2000003, 170000, 2000002, 170000, 2000004, 170000, 2000006, 170000, 2000005, 170000, 2022000, 170000, 2001001, 170000, 2001002, 170000, 2022003, 170000, 2001000, 170000, 2020014, 170000, 2020015, 170000, /*Use*/
+                  4003000, 250000, 4010003, 250000, 4010000, 250000, 4010006, 250000, 4010002, 250000, 4010005, 250000, 4010004, 250000, 4010001, 250000, 4020001, 250000, 4020002, 250000, 4020008, 250000, 4020007, 250000, 4020003, 250000, 4020000, 250000, 4020004, 250000, 4020005, 250000, 4020006, 250000 /*Etc*/);
+
+for (let i = 0; i < amount; i++) {
+    reactor.dropItem(10, 100, 1000000);
+}
