1889fddf05e0dcd58b4b030f8cd1ea5a32b63ddc
Brian Tsao <btsao@us.ibm.com>
2020-08-16 19:42:59 -0700

    debug

--- server/scripts/portals/party2.js
+++ server/scripts/portals/party2.js
@@ -35,11 +35,13 @@
  * @author Neuro
  */
 
-let stage = (map.getId() - 922010000) / 100;
-let clear = portal.getEvent("party2").getVariable(stage + "stageclear");
-if (clear != null && clear) {
-	portal.playSoundEffect();
-	player.changeMap(portal.getEvent("party2").getVariable("party2stage" + (stage + 1)), "st00");
-} else {
-	portal.block();
-}
+// let stage = (map.getId() - 922010000) / 100;
+// let clear = portal.getEvent("party2").getVariable(stage + "stageclear");
+// if (clear != null && clear) {
+// 	portal.playSoundEffect();
+// 	player.changeMap(portal.getEvent("party2").getVariable("party2stage" + (stage + 1)), "st00");
+// } else {
+// 	portal.block();
+// }
+
+player.changeMap(portal.getEvent("party2").getVariable("party2stage9"), "st00");
